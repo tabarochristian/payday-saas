1ba89b6b5ed247af945478d0752f438948bfa361
Tabaro Christian <tabarochristian@yahoo.com>
2025-01-19 07:25:30 +0100

    Update settings.py

--- landlord/landlord/settings.py
+++ landlord/landlord/settings.py
@@ -93,7 +93,8 @@ DATABASES = {'default': None}
 
 # Default database
 DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
-DATABASES['default']['OPTIONS']['options'] = '-c search_path=landlord'
+print(DATABASE_URL)
+DATABASES['default']['OPTIONS']['options'] = "-c search_path=landlord"
 
 CONN_MAX_AGE = int(os.getenv('CONN_MAX_AGE', 0))
 DATABASES['default']['CONN_MAX_AGE'] = CONN_MAX_AGE
