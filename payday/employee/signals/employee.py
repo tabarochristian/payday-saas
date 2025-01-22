b9c4b8a53118e9517e1275dd563b4158db3187eb
Tabaro Christian <tabarochristian@yahoo.com>
2025-01-22 14:18:31 +0100

    Update employee.py

--- payday/employee/signals/employee.py
+++ payday/employee/signals/employee.py
@@ -15,7 +15,7 @@ def employee_created(sender, instance, created, **kwargs):
     if Preference.get('CREATE_USER_ON_EMPLOYEE', True) and instance.email:
         user = instance.create_user()
 
-    if instance.photo is None: return
-    schema = TenantMiddleware.get_tenant()
-    print(f"Schema: {schema}, Registration Number: {instance.registration_number}")
-    setuserinfo.delay(schema, instance.registration_number)
+    if instance.photo:
+        schema = TenantMiddleware.get_tenant()
+        print(f"Schema: {schema}, Registration Number: {instance.registration_number}")
+        setuserinfo.delay(schema, instance.registration_number)
