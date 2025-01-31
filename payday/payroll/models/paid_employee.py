b488e6c7a5d0b119d920898a7061c7957f07c6e7
Tabaro Christian <tabarochristian@yahoo.com>
2025-01-31 11:22:55 +0100

    payday

--- payday/payroll/models/paid_employee.py
+++ payday/payroll/models/paid_employee.py
@@ -1,16 +1,36 @@
 from django.utils.translation import gettext as _
-from core.models.fields import ModelSelectField
 from employee.models.base import Employee
 from django.urls import reverse_lazy
+from core.models import fields
 from django.db import models
 
-class PaidEmployee(Employee):
-    photo = None
-    agreement, status = None, None
-    branch, grade, designation = None, None, None
-    direction, subdirection, service = None, None, None
 
-    payroll = ModelSelectField('payroll.payroll', verbose_name=_('paie'), blank=True, null=True, default=None, on_delete=models.SET_NULL)
+class PaidEmployee(Employee):
+    payroll = fields.ModelSelectField(
+        'payroll.payroll', 
+        verbose_name=_('paie'), 
+        blank=True, 
+        null=True, 
+        default=None, 
+        on_delete=models.SET_NULL
+    )
+
+    attendance = fields.IntegerField(
+        verbose_name=_('présence'), 
+        default=0
+    )
+
+    registration_number = fields.CharField(verbose_name=_('matricule'), blank=True, null=True, default=None)
+    agreement = fields.CharField(verbose_name=_('type de contrat'), blank=True, null=True, default=None)
+    status = fields.CharField(verbose_name=_('statut'), blank=True, null=True, default=None)
+
+    designation = fields.CharField(verbose_name=_('désignation'), blank=True, null=True, default=None)
+    branch = fields.CharField(verbose_name=_('site'), blank=True, null=True, default=None)
+    grade = fields.CharField(verbose_name=_('grade'), blank=True, null=True, default=None)
+
+    subdirection = fields.CharField(verbose_name=_('sous-direction'), blank=True, null=True, default=None)
+    direction = fields.CharField(verbose_name=_('direction'), blank=True, null=True, default=None)
+    service = fields.CharField(verbose_name=_('service'), blank=True, null=True, default=None)
 
     @property
     def name(self):
--- payday/payroll/signals/payroll.py
+++ payday/payroll/signals/payroll.py
@@ -4,7 +4,8 @@ from django.dispatch import receiver
 from payroll.models import Payroll, PaidEmployee
 from payroll.tasks import Payer
 
-from employee.models import Employee
+from employee.models import Employee, Attendance
+import pandas as pd
 
 @receiver(pre_save, sender=Payroll)
 def payroll_create(sender, instance, **kwargs):
@@ -21,8 +22,20 @@ def payroll_created(sender, instance, created, **kwargs):
     #Payer().delay(instance.id)
 
 def duplicate(payroll):
-    fields = [f.name for f in Employee._meta.get_fields()]
+
+    def get_field_name(name):
+        return name.split('__')[0]
+
+    fields = [f"{f.name}__name" if f.is_relation else f.name for f in Employee._meta.get_fields()]
     employees = Employee.objects.all().values(*fields)
+
+    df = pd.from_records(list(employees))
+    df.columns = [get_field_name(c) for c in df.columns]
+    df['attendance'] = 0
+
+    # fetch all attendance of the period of the payroll
+    attendances = Attendance.objects.filter(date__range=(payroll.start_date, payroll.end_date))
+    attendances = attendances.values('employee__registration_number', 'checked_at__date').annotate(attendances_count=Count('employee__name'))
 
     paid_employees = [PaidEmployee(**employee, attendances=0, payroll=payroll) for employee in employees]
     PaidEmployee.objects.bulk_create(paid_employees)
