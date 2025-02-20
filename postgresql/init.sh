60b8087b03605524f698bf974d11be709dfaf660
Tabaro Christian <tabarochristian@yahoo.com>
2025-02-20 19:57:40 +0100

    postgresql

--- postgresql/init.sh
+++ postgresql/init.sh
@@ -8,7 +8,7 @@ apt install -y curl gnupg lsb-release
 
 # Step 2: Add Dalibo Labs APT repository
 echo "Adding Dalibo Labs repository..."
-echo "deb [arch=amd64] http://apt.dalibo.org/labs/$(lsb_release -cs) $(lsb_release -cs)-dalibo main" > /etc/apt/sources.list.d/dalibo-labs.list
+echo "deb [arch=amd64] http://apt.dalibo.org/labs bullseye-dalibo main" > /etc/apt/sources.list.d/dalibo-labs.list
 curl -fsSL -o /etc/apt/trusted.gpg.d/dalibo-labs.gpg https://apt.dalibo.org/labs/debian-dalibo.gpg
 
 # Step 3: Install PostgreSQL Anonymizer
