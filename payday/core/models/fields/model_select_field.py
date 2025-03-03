38db17eff442939bc0a67abb2cebdeef4d17b88f
Tabaro Christian <christian@tabaro.me>
2025-03-03 17:05:51 +0100

    ModelSelectField

--- payday/core/models/fields/model_select_field.py
+++ payday/core/models/fields/model_select_field.py
@@ -3,21 +3,19 @@ from django.db import models
 from dal import autocomplete
 
 class ModelSelectField(models.ForeignKey):
-    on_delete = models.SET_NULL
-    default = None
-    null = True
-
     def __init__(self, *args, **kwargs):
-        self.on_delete = kwargs.pop('on_delete', self.on_delete)
-        self.default = kwargs.pop('default', self.default)
-        self.null = kwargs.pop('null', self.null)
         self.inline = kwargs.pop('inline', False)
-        super().__init__(null=self.null, on_delete=self.on_delete, default=self.default, *args, **kwargs)
+        
+        kwargs['on_delete'] = models.SET_NULL
+        kwargs['default'] = None
+        kwargs['null'] = True
+
+        super().__init__(*args, **kwargs)
     
     def formfield(self, **kwargs):
         to_field = getattr(self, 'foreign_related_fields', None)
         to_field = to_field[0].name if to_field else 'pk'
-        kwargs['widget'] = autocomplete.ModelSelect2(url=reverse_lazy('api:autocomplete', kwargs={
+        kwargs['widget'] = autocomplete.ModelSelect2(url=reverse_lazy('api:autocomplete', kwargs = {
             'to_field': to_field,
             'app': self.remote_field.model._meta.app_label,
             'model': self.remote_field.model._meta.model_name
