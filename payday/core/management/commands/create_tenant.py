69e12986d01fb9b3b8dc98e8b6f928f282817297
Tabaro Christian <christian@tabaro.me>
2025-05-15 00:41:44 +0800

    Update create_tenant.py

--- payday/core/management/commands/create_tenant.py
+++ payday/core/management/commands/create_tenant.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
 from django.conf import settings
 from typing import Optional
 import re, logging
