48e3be296c940b0a42b5799ed53521723192e1ea
Tabaro Christian <christian@tabaro.me>
2025-05-19 17:49:13 +0800

    Update change.py

--- payday/core/views/base/change.py
+++ payday/core/views/base/change.py
@@ -144,7 +144,8 @@ class Change(BaseView):
         form = self.filter_form(FormClass(request.POST, request.FILES, instance=obj))
         formsets = list(self.get_formsets(obj))
 
-        if not all(formset.is_valid() for formset in [form] + formsets):
+        _formsets = [form] + formsets
+        if not all(fs.is_valid() for fs in _formsets):
             for error in form.errors.values():
                 messages.warning(request, error)
 
