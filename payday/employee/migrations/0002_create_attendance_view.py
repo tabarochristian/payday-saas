aae9810a64e39a4a2e54de6f840fb1dae5633eff
Tabaro Christian <christian@tabaro.me>
2025-05-22 18:18:04 +0800

    Update 0002_create_attendance_view.py

--- payday/employee/migrations/0002_create_attendance_view.py
+++ payday/employee/migrations/0002_create_attendance_view.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
             """
             -- Create the view with grouped data
             DROP VIEW IF EXISTS employee_attendance;
-            CREATE VIEW OR REPLACE employee_attendance AS
+            CREATE VIEW employee_attendance AS
             SELECT
                 ROW_NUMBER() OVER () AS id, -- Generates an incremental ID
                 d.id AS device_id, -- Fetching the actual device_id from device_device
