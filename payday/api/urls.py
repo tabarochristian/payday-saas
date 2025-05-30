318f7f4cc471a85f549e499f0784aa3c37d92f38
Teddy Cubaka <teddycubaka96@gmail.com>
2025-05-30 08:43:07 +0100

    add a view to sync employee data with arafat

--- payday/api/urls.py
+++ payday/api/urls.py
@@ -1,24 +1,28 @@
+from .views import ApiViewSet
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import *
 
 app_name = 'api'
 
-from django.urls import path
-from .views import ApiViewSet
 
 urlpatterns = [
-    path('autocomplete/<str:app>/<str:model>/<str:to_field>', Autocomplete.as_view(), name='autocomplete'),
+    path('autocomplete/<str:app>/<str:model>/<str:to_field>',
+         Autocomplete.as_view(), name='autocomplete'),
 
     path('v1/<str:app>/<str:model>', ApiViewSet.as_view({
-        'get': 'list', 
+        'get': 'list',
         'post': 'create'
     }), name='list'),
 
     path('v1/<str:app>/<str:model>/<str:pk>', ApiViewSet.as_view({
-        'get': 'retrieve', 
-        'put': 'update', 
-        'patch': 'partial_update', 
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
         'delete': 'destroy'
     }), name='detail'),
+
+    path('sync/rollapp/employee', EmployeeSyncWithRollApp.as_view(),
+         name='employee-sync-api'),
 ]
