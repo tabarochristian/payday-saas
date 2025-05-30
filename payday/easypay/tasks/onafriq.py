82a9cccf24e571758bd7f37537b192c7b8d2bb3c
Teddy Cubaka <teddycubaka96@gmail.com>
2025-05-30 10:39:45 +0100

    config onfriq task

--- payday/easypay/tasks/onafriq.py
+++ payday/easypay/tasks/onafriq.py
@@ -1,3 +1,23 @@
+from celery import shared_task
 import requests
-import random
-import json
+from payroll.models import PaidEmployee
+from django.conf import settings
+
+
+@shared_task
+def send_mobile_payment(payload):
+    try:
+        print('on y est ............................................')
+        print("Envoi de la requête de paiement à Onafriq avec les données:", payload)
+        # headers = {
+        #     "Authorization": f"Token {settings.ONAFRIQ_TOKEN}",
+        #     "Content-Type": "application/json"
+        # }
+        # response = requests.post(
+        #     "https://api.onafriq.com/api/v5/payments", json=payload, headers=headers, timeout=10)
+        # response.raise_for_status()
+        # # Tu peux enregistrer ici une confirmation ou log
+        # print("Paiement réussi:", payload['full_name'])
+        # return {"status": "success", "response": response.json()}
+    except requests.RequestException as e:
+        return {"status": "error", "error": str(e)}
